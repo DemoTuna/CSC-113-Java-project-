d2247f6735d4e260d749b8836ba96b8252ab8b2e
demaa <demaa@LAPTOP-ASL610M3>
2024-02-26 21:29:24 +0300

    Modifying1 Doc class

--- LawFirmProject/src/LawFirmProject/Document.java
+++ LawFirmProject/src/LawFirmProject/Document.java
@@ -2,6 +2,73 @@ package LawFirmProject;
 
 public class Document {
 
+	private String trackingNumber ;
+	private String title ;
+	private char type ;
+	private String content ;
+	private char accessLevel ;
 	
 	
+	public Document(String trackingNumber, String title, char type, String content, char accessLevel) {
+		
+		this.trackingNumber = trackingNumber;
+		this.title = title;
+		this.type = type;
+		this.content = content;
+		this.accessLevel = accessLevel;
+	}
+
+
+	public String getTrackingNumber() {
+		return trackingNumber;
+	}
+
+
+	public void setTrackingNumber(String trackingNumber) {
+		this.trackingNumber = trackingNumber;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public char getType() {
+		return type;
+	}
+
+
+	public void setType(char type) {
+		this.type = type;
+	}
+
+
+	public String getContent() {
+		return content;
+	}
+
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+
+	public char getAccessLevel() {
+		return accessLevel;
+	}
+
+
+	public void setAccessLevel(char accessLevel) {
+		this.accessLevel = accessLevel;
+	}
+	
+	
+	
+
 }
