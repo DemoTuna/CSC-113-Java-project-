47c7f11d8dea6e1a560753147605443ea8be30ed
iRazan4 <161145992+iRazan4@users.noreply.github.com>
2024-02-27 14:45:57 +0300

    Update Case.java

--- LawFirmProject/src/LawFirmProject/Case.java
+++ LawFirmProject/src/LawFirmProject/Case.java
@@ -1,5 +1,47 @@
 package LawFirmProject;
-kk
+
+// Abstract class representing a legal case
 public class Case {
+// Fields to store case information
+	protected String caseNumber;      
+	protected char status;           // Status of the case:Active (A), Pending (P), Closed (C)
+	protected double legalExpenses;
+   protected Document document;
+
+
+	
+   // Constructor to initialize the case fields
+	public Case(String caseNumber, char status, double legalExpenses , Document document) {
+    this.caseNumber = caseNumber;
+    this.status = status;
+    this.legalExpenses = legalExpenses;
+    this.document=document;    
+}
+	
+	
+	// Abstract method to calculate legal expenses (to be implemented by subclasses)
+	public abstract double calculateLegalExpenses();
+   
+	// Method to display case information
+	public void display(){
+       System.out.println("Case Number: " + caseNumber);
+  
+        System.out.print("Status: ");
+        switch (status) {
+            case 'A':
+                System.out.println("Active");
+                break;
+            case 'P':
+                System.out.println("Pending");
+                break;
+            case 'C':
+                System.out.println("Closed");
+                break;
+            default:
+                System.out.println("Unknown"); // Print "Unknown" for any other status code
+        }
+        System.out.println("Legal Expenses: " + legalExpenses);     
+    }
+    
 
 }
