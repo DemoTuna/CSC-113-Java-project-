7415e0ec9c0c8138a92d257bec22fa1b1e86884e
Haneenmaa <161174473+Haneenmaa@users.noreply.github.com>
2024-03-09 04:32:02 +0300

    Update Criminal.java

--- LawFirmProject/src/LawFirmProject/Criminal.java
+++ LawFirmProject/src/LawFirmProject/Criminal.java
@@ -1,5 +1,38 @@
 package LawFirmProject;
 
-public class Criminal {
 
+public class Criminal extends Case {
+// attribute
+private char crimeTybe; // // Crime type: Cyber crime (C), Money laundering (M), Robbery (R), Homicide (H)
+
+
+//Parameterized constructor
+public Criminal(String caseNumber, char status, double legalExpenses , Document document ,  char crimeTybe) {
+super(caseNumber, status, legalExpenses, document);
+this.crimeTybe = crimeTybe;
+
+}
+
+
+ //method to calculate legal expenses for criminal cases
+/*public  double calculateLegalExpenses() {
+
+
+}*/
+
+
+
+// Setter method 
+public void setCrimeTybe(char crimeTybe) {
+this.crimeTybe = crimeTybe; 
+}
+
+//Getter method
+public char getCrimeTybe() {
+return crimeTybe;
+
+
+
+
+}
 }
