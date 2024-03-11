a7ffe67242f9b464570d93f308d93a0986eae0ce
DemoTuna <161108798+DemoTuna@users.noreply.github.com>
2024-03-11 16:24:55 +0300

    Update Criminal.java
    last

--- LawFirmProject/src/LawFirmProject/Criminal.java
+++ LawFirmProject/src/LawFirmProject/Criminal.java
@@ -2,37 +2,85 @@ package LawFirmProject;
 
 
 public class Criminal extends Case {
+	
 // attribute
-private char crimeTybe; // // Crime type: Cyber crime (C), Money laundering (M), Robbery (R), Homicide (H)
+	
+private char crimeTybe; // // Crime type can be : Cyber crime (C), Money laundering (M), Robbery (R), Homicide (H)
 
-
+    
 //Parameterized constructor
-public Criminal(String caseNumber, char status, double legalExpenses , Document document ,  char crimeTybe) {
-super(caseNumber, status, legalExpenses, document);
-this.crimeTybe = crimeTybe;
+
+    public Criminal(String caseNumber, char status,  Client client ,int documentSize ,  char crimeTybe) {
+    super(caseNumber, status, client,documentSize);  // Call superclass constructor
+    this.crimeTybe = crimeTybe;
 
 }
 
+ //Method to calculate legal expenses for criminal cases  
+    public  double calculateLegalExpenses() {
+        
+    	switch (crimeTybe) {
+        case 'C':
+        	legalExpenses = 40000 ;
+          break;
+        case 'M':
+       	 	legalExpenses = 35000 ;
+          break;
+        case 'R':
+       	 	legalExpenses = 30000 ;
+          break;
+        case 'H':
+       	 	legalExpenses = 50000 ;
+          break;
+        default:
+          System.out.println("Invaled Crime Type ");
+        } 	
+    	
+    	if (status == 'P' || status == 'A' )
+    		legalExpenses = legalExpenses * 0.5 ;
+    	else
+    		if(status == 'L')
+    		   legalExpenses = legalExpenses * 0.25 ;
+    	
+    	
+	         return legalExpenses;
+    }
 
- //method to calculate legal expenses for criminal cases
-/*public  double calculateLegalExpenses() {
 
-
-}*/
-
-
-
-// Setter method 
-public void setCrimeTybe(char crimeTybe) {
-this.crimeTybe = crimeTybe; 
+    // setters & getters
+	public void setCrimeTybe(char crimeTybe) {
+		this.crimeTybe = crimeTybe; 
 }
 
-//Getter method
-public char getCrimeTybe() {
-return crimeTybe;
+	public char getCrimeTybe() {
+		return crimeTybe;
+	}
 
 
 
-
+	public String toString() {
+		String crimeTybeString = "";
+		 
+		 switch (crimeTybe) {
+        case 'C':
+        	crimeTybeString = "Cyber crime";
+          break;
+        case 'M':
+        	crimeTybeString = "Money laundering";
+          break;
+        case 'R':
+        	crimeTybeString ="Robbery";
+          break;
+        case 'H':
+        	crimeTybeString ="Homicide";
+          break;
+        default:
+          System.out.println("Invaled Crime Type ");
+		 }
+	String sum = "";
+	sum += super.toString();
+	sum+= "\nCase Type: Criminal Case \nCrime Tybe: " + crimeTybeString + "\nLegal Expenses: " + calculateLegalExpenses();
+	return sum ;
 }
+
 }
