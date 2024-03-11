4e3b18f7c2eb98c59f24185edb4cc532f8b04fcb
DemoTuna <161108798+DemoTuna@users.noreply.github.com>
2024-03-11 16:23:21 +0300

    Update Case.java
    last

--- LawFirmProject/src/LawFirmProject/Case.java
+++ LawFirmProject/src/LawFirmProject/Case.java
@@ -7,73 +7,72 @@ public abstract class Case {
   // Attributes
    
    protected String caseNumber;      
-   protected char status;           // Status of the case:Active (A), Pending (P), Closed (C)
+   protected char status;      // Status of the case can be:  Active (A), Pending (P), Lost (L), Won (W) 
    protected double legalExpenses;
    protected Client client;
-   protected Document[] documents;
+   protected Document[] documents; // all arrays name 
    protected int numberOfDocuments;
 
 
 	
    //Parameterized constructor
    
-    public Case(String caseNumber, char status, Client client,int size) {
+    public Case(String caseNumber, char status, Client client,int documentSize) {
     this.caseNumber = caseNumber;
     this.status = status;
     this.client=client; 
-    this.documents=new Document[size];
+    this.documents=new Document[documentSize];
     this.legalExpenses = 0; 
     this.numberOfDocuments=0;
    }
    
    // Method to add a document to the case
-   public void addDocument(Document d){
-     if(numberOfDocuments<documents.length){
-         documents[numberOfDocuments]=d;
-         numberOfDocuments++;
+   public boolean addDocument(Document document){
+     if(numberOfDocuments < documents.length){
+         documents[numberOfDocuments++] = document;
+         return true ;
      }
-     else
-       System.out.println("Cannot add more documents.");
+     return false ;
    
    }
    
    // Method to search for a document by its tracking number
-   public int searchDocument(int trackingNumber){
-     for(int i=0;i<numberOfDocuments;i++){
-         if(documents[i].getTrackingNumber()==trackingNumber)
-              return i;
+   public Document searchForDocument(String trackingNum){
+     for(int i=0 ; i < numberOfDocuments ; i++){
+         if(documents[i].getTrackingNumber().equals(trackingNum))
+              return documents[i] ;
          }
-              return -1;
-     
-   
+             return null ;
    }
    
-   
-   // Method to remove a document 
-   public void removeDocument(int id){
-      int index=searchDocument(id);
-      if(index==-1)
-           System.out.println("Case is not found.");
-       else{
-           documents[index]=documents[numberOfDocuments-1];
-           numberOfDocuments--;
-           
+   // Method to delete a document 
+   public boolean deleteDocument(String trackingNum){
+	   for(int i=0 ; i < numberOfDocuments ; i++)
+	         if(documents[i].getTrackingNumber().equals(trackingNum)) {
+	        	 documents[i] = documents[numberOfDocuments-1];
+	        	 documents[--numberOfDocuments] = null ;
+	        	 return true ; 
            }
-   
-   
+	         return false ;
    }
-	
-	
-	   
+	 
+   public void listAllDocuments() {
+		for(int i = 0 ; i < numberOfDocuments; i++) {
+			System.out.println("\n"+(i+1) + " :\n");
+			System.out.println("\n" + documents[i] + "\n");
+		}
+   }
+   
    public abstract double calculateLegalExpenses();     // Abstract method to calculate legal expenses (to be implemented by subclasses).
   
    
-   public abstract int calculateCompletionTime();       //Abstract method to calculates the estimated completion time for the case.
    
    // toString method 
    
    public String toString() {
-    String statusString;
+	   
+    String statusString = "" ;
+    
      switch (status) {
         case 'A':
             statusString = "Active";
@@ -81,20 +80,65 @@ public abstract class Case {
         case 'P':
             statusString = "Pending";
             break;
-        case 'C':
-            statusString = "Closed";
+        case 'L':
+            statusString = "Lost";
+            break;
+        case 'W':
+            statusString = "Won";
             break;
         default:
-            statusString = "Unknown";
-            break;
+            System.out.println("Invalid Status");
     }
+     
     String documentsInfo = "";
     for (int i = 0; i < numberOfDocuments; i++) {
-           documentsInfo +=(i+1)+" "+ documents[i] + "\n";
+           documentsInfo +="Document "+(i+1)+" : "+ documents[i] + "\n";  // list all docs method
 }    
 
-    return "Case Number: " + caseNumber + ", Status: " + statusString  + "\n " + client +"\n" + documentsInfo;
+    return "Case Number: " + caseNumber + "\nStatus: " + statusString  + "\n " + client +"\n" + documentsInfo;
     
+}
+   
+   // setters & getters
+
+   public String getCaseNumber() {
+	return caseNumber;
+}
+
+   public void setCaseNumber(String caseNumber) {
+	this.caseNumber = caseNumber;
+}
+
+   public char getStatus() {
+	return status;
+}
+
+   public void setStatus(char status) {
+	this.status = status;
+}
+
+   public double getLegalExpenses() {
+	return legalExpenses;
+}
+
+   public void setLegalExpenses(double legalExpenses) {
+	this.legalExpenses = legalExpenses;
+}
+
+   public Client getClient() {
+	return client;
+}
+
+   public void setClient(Client client) {
+	this.client = client;
+}
+
+   public int getNumberOfDocuments() {
+	return numberOfDocuments;
+}
+
+   public void setNumberOfDocuments(int numberOfDocuments) {
+	this.numberOfDocuments = numberOfDocuments;
 }
  
        
@@ -104,6 +148,3 @@ public abstract class Case {
     
         
   
-    
-
-     
